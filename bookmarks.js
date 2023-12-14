7dfeae5a469828102c5bbf887354e49650f80c0d
Hai HUANG <101720804+maple367@users.noreply.github.com>
2023-12-14 23:29:05 +0800

    modified:   bookmarks.js 	deleted:    favicon.png 	modified:   index.html 	modified:   styles.css

--- bookmarks.js
+++ bookmarks.js
@@ -1,47 +1,39 @@
 // Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
   {
-    title: "Daily",
+    title: "Main",
     links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
-      { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
+      { name: "Maple's Blog", url: "https://www.maple367.eu.org/" },
+      { name: "Cloud Flare", url: "https://dash.cloudflare.com/" },
+      { name: "Vercel", url: "https://vercel.com/" },
+      { name: "GitHub", url: "https://github.com/" },
+      { name: "More Site", url: "https://www.maple367.eu.org/links/" },
     ],
   },
   {
-    title: "Media",
+    title: "Search",
     links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Bing School", url: "https://www.bing.com/school" },
+      { name: "Google", url: "https://www.google.com.hk/" },
       { name: "Crunchyroll", url: "https://crunchyroll.com" },
-      {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
-      },
     ],
   },
   {
-    title: "Reddit",
+    title: "Amusement",
     links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
+      { name: "Bilibili", url: "https://www.bilibili.com/" },
       {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
+        name: "YouTube",
+        url: "https://www.youtube.com/",
       },
-      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
-      {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
-      },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
-    ],
-  },
-  {
-    title: "Social",
-    links: [
       { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
+    ],
+  },
+  {
+    title: "Tools",
+    links: [
+      { name: "MusicMini", url: "/MusicMini" },
+      { name: "Z-Library", url: "https://zh.singlelogin.re/" },
     ],
   },
 ];
